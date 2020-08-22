1b0a390092ec4142861c301e340dbb0ba82dc621
Michael269Yang <michael777yang@gmail.com>
2020-08-22 00:56:06 -0400

    Edits

--- SentimentTester.py
+++ SentimentTester.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('training_data.csv')
 
@@ -9,3 +10,7 @@ print(df.head())
 
 new_df = df[:1000]
 print(new_df.size)
+
+X = new_df['Text']
+y = new_df['Sentiment']
+
